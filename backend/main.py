93c65b0b2eb3e135ffda1486d18da0f4feb89f74
ramenisneat <rahulmyana42@gmail.com>
2025-03-30 08:16:22 -0500

    multithreading

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,8 @@ import os
 from spotipy.oauth2 import SpotifyOAuth
 from starlette.middleware.sessions import SessionMiddleware
 from openai import OpenAI
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 load_dotenv()
 
@@ -17,9 +19,6 @@ app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SECRET_KEY"))
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
-
-#test3
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
@@ -37,8 +36,6 @@ sp_oauth = SpotifyOAuth(
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
    return templates.TemplateResponse("index.html", {"request": request})
-
-
 
 
 def get_track_uri(item, sp):
@@ -68,7 +65,7 @@ def gen_playlist(request: Request, keywords: str = Form(...)):
     messages=[
         {
             "role": "developer",
-            "content": "You are a playlist generator api. Users will prompt you with a list of buzzwords. It is you rjob to create a spotify list of songs that mathches the vibes given in the list of buzzwords. There should be no other commentary in your response, only include the list of songs. Recommend atleast 40 songs. Shuffle it. Make a title for it and add it to the start of the list, Make the title witty as can be and in all lowercase. Make sure to not add any special characters to the title. The first line should be the title and the rest of the lines should be the songs. The songs should not be numbered and should be in the format of {song_name}-{artist_name}"
+            "content": "You are a playlist generator api. Users will prompt you with a list of buzzwords. It is you rjob to create a spotify playlist of songs that mathches the vibes given in the list of buzzwords. There should be no other commentary in your response, only include the list of songs. Recommend atleast 30 songs. Shuffle it. Make a title for it and add it to the start of the list, Make the title witty as can be and in all lowercase. The title can even be nonsensical if it fits the vibes. Make sure to not add any special characters to the title. The first line should be the title and the rest of the lines should be the songs. The songs should not be numbered and should be in the format of {song_name}-{artist_name}"
         }
         ,
         {
@@ -86,15 +83,28 @@ def gen_playlist(request: Request, keywords: str = Form(...)):
    print(response)
 
    tracks = []
-   for i in response[1:]:
-      if i == "":
-         continue
-      track = get_track_uri(i, sp)
-      # print(track)
-      if (track):
-         tracks.append(track)
+   # for i in response[1:]:
+   #    if i == "":
+   #       continue
+   #    track = get_track_uri(i, sp)
+   #    if track:
+   #       tracks.append(track)
+
+   with ThreadPoolExecutor(max_workers=10) as executor:
+      future_to_query = {executor.submit(get_track_uri, query, sp): query for query in response}
+      for future in as_completed(future_to_query):
+         query = future_to_query[future]
+         try:
+               track_id = future.result()
+               if track_id:
+                  tracks.append(track_id)
+               else:
+                  print(f"No track found for: {query}")
+         except Exception as e:
+               print(f"Error processing {query}: {e}")
+
    # print(tracks)
-   if (len(tracks) == 0):
+   if len(tracks) == 0:
       tracks = ["spotify:track:1eajP3G86GZ0VEZgF8DeJS"]
       playlist_name = "No worky :("
    else:
